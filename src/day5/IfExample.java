746dbd66c36dad3a1034f7151c2dde9df3431873
sugwan <skhrnt2945@gmail.com>
2025-04-05 23:35:32 +0900

    feat: day5 if 문과 switch 문

---
+++ src/day5/IfExample.java
@@ -0,0 +1,15 @@
+package src.day5;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int score = 93;
+
+        if (score >= 90) {
+            System.out.println("점수가 90점 이상입니다");
+        } else if (score >= 80) {
+            System.out.println("점수가 80점 이상 그리고 90점 미만입니다");
+        } else {
+            System.out.println("점수가 80점 미만입니다");
+        }
+    }
+}
