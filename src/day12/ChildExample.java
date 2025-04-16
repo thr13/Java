af50fe4531f452baff04e27543ac7cbcc13f7931
sugwan <skhrnt2945@gmail.com>
2025-04-16 09:31:16 +0900

    feat: day12 강제타입변환2

--- src/day12/ChildExample.java
+++ src/day12/ChildExample.java
@@ -6,7 +6,7 @@ public class ChildExample {
         Parent parent = new Child();
 
         // 부모클래스 타입으로 필드, 메소드 사용
-        parent.field1 = "데이터";
+        parent.field1 = "데이터1";
         parent.method1();
         parent.method2();
 
@@ -16,9 +16,13 @@ public class ChildExample {
         parent.method3();
          */
 
-        // 자식클래스 타입으로 강제타입변환
-        Child child = (Child) parent;
-        child.field2 = "data2";
+        // 자식클래스 타입으로 강제타입변환 -> 자식 클래스의 필드와 메소드 사용가능
+        Child child = (Child) parent; // 타입캐스팅은 (자식클래스) 부모클래스타입변수 형식으로 사용한다
+        child.field2 = "데이터2";
         child.method3();
+
+        // 강제타입변환 이후에도 부모클래스의 필드와 메소드 사용가능함
+        child.field1 = "수정된데이터1";
+        child.method2();
     }
 }
