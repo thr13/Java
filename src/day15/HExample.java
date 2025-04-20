ed3db891752ec8ba509d25fff7489fb2da59fa0e
sugwan <skhrnt2945@gmail.com>
2025-04-20 21:06:13 +0900

    feat: day15 중첩된 클래스 - 내부 클래스에서 바깥 객체 사용

---
+++ src/day15/HExample.java
@@ -0,0 +1,11 @@
+package src.day15;
+
+public class HExample {
+    public static void main(String[] args) {
+        // 바깥 클래스 H 객체 생성
+        H h = new H();
+
+        // 바깥 클래스의 인스턴스 메소드로 내부 클래스 사용
+        h.usdI();
+    }
+}
