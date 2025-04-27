22b4dae0f2376c94cbdf5eb94163bcab0e4875cd
sugwan <skhrnt2945@gmail.com>
2025-04-27 18:28:20 +0900

    feat: lombok 라이브러리

--- src/day18/Member.java
+++ src/day18/Member.java
@@ -1,8 +1,20 @@
 package src.day18;
 
-public class Member {
-    public String id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+@Data
+//@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private final String id; // @NoArgsConstructor 가 있다면 final 필드가 초기화 되지 않을 경우 컴파일 에러 발생한다
+    @NonNull
+    String name;
+    private int age;
+
+    /*
     public Member(String id) {
         this.id = id;
     }
@@ -16,4 +28,5 @@ public class Member {
         }
         return false;
     }
+    */
 }
