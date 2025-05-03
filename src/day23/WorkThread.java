44103f6ea2988a8463e45ae0619cc67f3088c557
sugwan <skhrnt2945@gmail.com>
2025-05-03 18:13:34 +0900

    feat: yield() 메소드

---
+++ src/day23/WorkThread.java
@@ -0,0 +1,20 @@
+package src.day23;
+
+public class WorkThread extends Thread {
+    public boolean work = true;
+
+    public WorkThread(String name) {
+        setName(name);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            if(work) {
+                System.out.println(getName() + ": 작업처리");
+            } else {
+                Thread.yield();
+            }
+        }
+    }
+}
