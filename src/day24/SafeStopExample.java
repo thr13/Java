ddb66222c3fb7f6e4dd5f98794b709cd93a131bd
sugwan <skhrnt2945@gmail.com>
2025-05-04 15:49:21 +0900

    feat: 필드와 setter 를 이용하여 스레드 종료

---
+++ src/day24/SafeStopExample.java
@@ -0,0 +1,15 @@
+package src.day24;
+
+public class SafeStopExample {
+    public static void main(String[] args) {
+        PrintThread printThread = new PrintThread();
+        printThread.start(); // 작업 스레드 시작
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        printThread.setStop(true); // 스레드를 종료하기 위해 stop 필드값을 변경하는 메소드 호출
+    }
+}
