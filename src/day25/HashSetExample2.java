c00e0b4145a55f7cfbe12e9c4bf75a65fa7e43be
sugwan <skhrnt2945@gmail.com>
2025-05-06 00:35:43 +0900

    feat: HashSet 예제

---
+++ src/day25/HashSetExample2.java
@@ -0,0 +1,21 @@
+package src.day25;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample2 {
+    public static void main(String[] args) {
+        Set<Member> set = new HashSet<>();
+
+        set.add(new Member("홍길동", 30));
+        System.out.println("hashCode: " + set.hashCode() );
+        System.out.println("equals: " + set.equals(set) );
+        System.out.println();
+
+        set.add(new Member("홍길동", 30)); // Member 인스턴스는 다르지만 동등 객체이므로 중복 저장하지 않고 무시됨
+        System.out.println("hashCode: " + set.hashCode() );
+        System.out.println();
+
+        System.out.println("총 객체 수: " + set.size());
+    }
+}
---
+++ src/day25/Member.java
@@ -0,0 +1,27 @@
+package src.day25;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // hashCode 메소드 오버라이딩 - name 과 age 값이 같으면 동일한 hashCode 가 리턴
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age;
+    }
+
+    // equals 메소드 오버라이딩 - name 과 age 값이 같으면 true 가 리턴
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member target) {
+            return target.name.equals(name) && (target.age==age);
+        } else {
+            return false;
+        }
+    }
+}
