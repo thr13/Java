f309ba675fb27550c838763ceb2c003d934392d6
sugwan <skhrnt2945@gmail.com>
2025-05-06 12:19:42 +0900

    docs: 스택과 큐 예제

---
+++ src/day25/Coin.java
@@ -0,0 +1,13 @@
+package src.day25;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
