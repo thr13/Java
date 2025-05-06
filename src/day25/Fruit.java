ec4c9970a5d173db3e9fa63151f4175389965f8a
sugwan <skhrnt2945@gmail.com>
2025-05-06 11:56:26 +0900

    feat: Comparable, Comparator 예제

---
+++ src/day25/Fruit.java
@@ -0,0 +1,11 @@
+package src.day25;
+
+public class Fruit {
+    public String name;
+    public int price;
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
