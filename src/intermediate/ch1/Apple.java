f9ec153d14fc11e1976398b7a9b3313286b4e4af
sugwan <skhrnt2945@gmail.com>
2025-05-23 23:30:22 +0900

    feat: 자바8 스트림 처리 예시1 자바8 스트림 도입부 설명

---
+++ src/intermediate/ch1/Apple.java
@@ -0,0 +1,34 @@
+package src.intermediate.ch1;
+
+public class Apple {
+    private int weight = 0; // 무게
+    private String color = ""; // 색상
+
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @SuppressWarnings("boxing")
+    @Override
+    public String toString() {
+        return String.format("Apple{color='%s', weight=%d}", color, weight);
+    }
+
+}
