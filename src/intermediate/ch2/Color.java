c2cd6c1c5ffa5b1ad3927207a03d7ead608799c1
sugwan <skhrnt2945@gmail.com>
2025-05-26 22:30:34 +0900

    docs: 자바8 람다식 예제

---
+++ src/intermediate/ch2/Color.java
@@ -0,0 +1,7 @@
+package src.intermediate.ch2;
+
+// 사과의 색
+public enum Color {
+    RED,
+    GREEN
+}
---
+++ src/intermediate/ch2/Predicate.java
@@ -0,0 +1,5 @@
+package src.intermediate.ch2;
+
+public interface Predicate<T>{
+    boolean test(T t);
+}
